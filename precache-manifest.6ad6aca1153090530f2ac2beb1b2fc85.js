30d92bfd495158afe3d834966e124045ab91f0ad
unknown <dragostinj@gmail.com>
2019-01-21 11:17:36 +0200

    Updates

---
+++ precache-manifest.6ad6aca1153090530f2ac2beb1b2fc85.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ffe29e424251ad30090",
+    "url": "/hooks-simple/static/js/runtime~main.9ffe29e4.js"
+  },
+  {
+    "revision": "139fa2ee22c1bcbc5e35",
+    "url": "/hooks-simple/static/js/main.139fa2ee.chunk.js"
+  },
+  {
+    "revision": "95fd558d7701b791f242",
+    "url": "/hooks-simple/static/js/1.95fd558d.chunk.js"
+  },
+  {
+    "revision": "9da421209cf38f594bdbe5f960a2304d",
+    "url": "/hooks-simple/index.html"
+  }
+];
